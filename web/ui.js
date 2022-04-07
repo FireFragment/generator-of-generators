ff04981a2802b7a0c2648faa2bbd47486550c8eb
FireFragment <55660550+FireFragment@users.noreply.github.com>
2022-04-07 19:16:15 +0200

    Add text to DOM + regenerate on space press

--- web/ui.js
+++ web/ui.js
@@ -1,2 +1,12 @@
-while (true)
-    alert(Generate(generator));
+
+function Regenerate() {
+    document.getElementById("generated").innerText = Generate(generator);
+
+}
+
+Regenerate();
+
+document.addEventListener('keydown', function(e) {
+    if (e.which == 32)
+        Regenerate();
+});
