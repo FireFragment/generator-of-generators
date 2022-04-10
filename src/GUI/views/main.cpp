c55c402c844e0045352ca86bc6f3671c04c32373
FireFragment <55660550+FireFragment@users.noreply.github.com>
2022-04-10 11:35:35 +0200

    Log avialable styles and set breeze

--- src/GUI/views/main.cpp
+++ src/GUI/views/main.cpp
@@ -1,15 +1,18 @@
 #include <QApplication>
 #include <QMainWindow>
 #include <QLayout>
+#include <iostream>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QStyleFactory>
 
 using namespace GoG::GUI;
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-
+    app.setStyle(QStyleFactory::create("Breeze"));
+    std::cout << QStyleFactory::keys().join(" ").toStdString();
     MainWindow w;
 
     w.show();
