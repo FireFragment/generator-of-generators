1fe18d219c82dff77be4ac91409de613482c3152
FireFragment <55660550+FireFragment@users.noreply.github.com>
2022-04-16 11:14:59 +0200

    Log information about icons to console
    To debug missing icons on windows

--- src/GUI/views/main.cpp
+++ src/GUI/views/main.cpp
@@ -11,8 +11,17 @@ using namespace GoG::GUI;
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    
     app.setStyle(QStyleFactory::create("Breeze"));
+    std::cout << "QStyleFactory::keys\n";
     std::cout << QStyleFactory::keys().join(" ").toStdString();
+    
+    std::cout << "Original QIcon::themeName: " << QIcon::themeName();
+    std::cout << "QIcon::themeSearchPaths\n";
+    std::cout << QIcon::themeSearchPaths().join(" ").toStdString();
+    QIcon::setThemeName( "breeze" );
+    std::cout << "QIcon::themeName after changing to breeze: " << QIcon::themeName();
+    
     MainWindow w;
 
     w.show();
