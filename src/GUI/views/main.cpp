4aa91784fa5bebc611d30ed78a024124d1678f75
FireFragment <55660550+FireFragment@users.noreply.github.com>
2022-05-12 18:10:52 +0200

    Force breeze icons on Windows and MacOS

--- src/GUI/views/main.cpp
+++ src/GUI/views/main.cpp
@@ -11,7 +11,11 @@ using namespace GoG::GUI;
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-    
+
+#if defined(Q_OS_WIN32) || defined(Q_OS_MAC)
+    QIcon::setThemeName("breeze");
+#endif
+
     MainWindow w;
     w.show();
 
